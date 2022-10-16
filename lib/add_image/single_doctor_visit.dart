fd47eb55cfbe8a288b5fd9f01384e23a4f833548
Madhur Shrestha <madhurstha@gmail.com>
2022-10-16 23:22:18 +0545

    Adding clickable containers for doctor visits and lab reports

---
+++ lib/add_image/single_doctor_visit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingleDoctorVisit extends StatefulWidget {
+  const SingleDoctorVisit({Key? key}) : super(key: key);
+
+  @override
+  State<SingleDoctorVisit> createState() => _SingleDoctorVisitState();
+}
+
+class _SingleDoctorVisitState extends State<SingleDoctorVisit> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/add_image/single_lab_report.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingleLabReport extends StatefulWidget {
+  const SingleLabReport({Key? key}) : super(key: key);
+
+  @override
+  State<SingleLabReport> createState() => _SingleLabReportState();
+}
+
+class _SingleLabReportState extends State<SingleLabReport> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
