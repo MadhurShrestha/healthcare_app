b45ab0caa3beb19a5e797e03fa6e7157c67cbb96
Madhur Shrestha <madhurstha@gmail.com>
2022-10-21 22:34:43 +0545

    Pinch to zoom feature added for photos

--- lib/add_image/single_doctor_visit.dart
+++ lib/add_image/single_doctor_visit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinch_zoom_release_unzoom/pinch_zoom_release_unzoom.dart';
 
 class SingleDoctorVisit extends StatefulWidget {
   const SingleDoctorVisit({Key? key}) : super(key: key);
@@ -15,26 +16,30 @@ class _SingleDoctorVisitState extends State<SingleDoctorVisit> {
         title: Text('Neurosurgeon Visit'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Image.network(
-              'https://www.nejm.org/na101/home/literatum/publisher/mms/journals/content/nejm/1999/nejm_1999.341.issue-7/nejm199908123410703/production/images/img_medium/nejm199908123410703_f1.jpeg',
-              height: 400,
-              width: 400,
-            ),
-            SizedBox(height: 10),
-            Text('Name: Saurabh Khatiwada'),
-            SizedBox(height: 10),
-            Text('Date: 2078/05/02'),
-            SizedBox(height: 10),
-            Text('Reason for visit: TG Increase while regular checkup'),
-            SizedBox(height: 10),
-            Text('Continue dosage of telsamartin. Follow up after 3 months'),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Text('Name: Saurabh Khatiwada'),
+              SizedBox(height: 10),
+              Text('Date: 2078/05/02'),
+              SizedBox(height: 10),
+              Text('Reason for visit: TG Increase while regular checkup'),
+              SizedBox(height: 10),
+              Text('Continue dosage of telsamartin. Follow up after 3 months'),
+              Center(
+                child: PinchZoomReleaseUnzoomWidget(
+                  fingersRequiredToPinch: -1,
+                  child: Image.network(
+                      'https://www.nejm.org/na101/home/literatum/publisher/mms/journals/content/nejm/1999/nejm_1999.341.issue-7/nejm199908123410703/production/images/img_medium/nejm199908123410703_f1.jpeg'),
+                  zoomChild:
+                      Container(height: 500, width: 500, color: Colors.red),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/add_image/single_lab_report.dart
+++ lib/add_image/single_lab_report.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinch_zoom_release_unzoom/pinch_zoom_release_unzoom.dart';
 
 class SingleLabReport extends StatefulWidget {
   const SingleLabReport({Key? key}) : super(key: key);
@@ -15,19 +16,35 @@ class _SingleLabReportState extends State<SingleLabReport> {
         title: Text('NRL Package'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Image.network(
-              'https://www.nejm.org/na101/home/literatum/publisher/mms/journals/content/nejm/1999/nejm_1999.341.issue-7/nejm199908123410703/production/images/img_medium/nejm199908123410703_f1.jpeg',
-              height: 200,
-              width: 200,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Text('Date: 2079/05/05'),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: PinchZoomReleaseUnzoomWidget(
+                  fingersRequiredToPinch: -1,
+                  child: Image.network(
+                      'https://www.nejm.org/na101/home/literatum/publisher/mms/journals/content/nejm/1999/nejm_1999.341.issue-7/nejm199908123410703/production/images/img_medium/nejm199908123410703_f1.jpeg'),
+                  zoomChild:
+                      Container(height: 500, width: 500, color: Colors.red),
+                ),
+              )
+            ],
+          ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit_outlined), label: '1'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarm_rounded), label: '2')
+        ],
       ),
     );
   }
